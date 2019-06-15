6764cd0cb4b0227824573480494728b2fd2b6b7e
adebola <bola@ringley.co.uk>
2019-06-15 16:39:33 +0100

    fix bugs

--- content/js/card.js
+++ content/js/card.js
@@ -1,3 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    objectFitImages();
-});
+let cardContainer = document.querySelector('#card-container');
+let modal = document.querySelector('.modal');
+let cancelBtn = document.querySelector('.cancel-btn');
+let deleteBtn = document.querySelector('.delete-btn');
+let productToDelete = null;
+
+// Handle heart icon click
+const handleCardControllers = (e) => {
+    if(e.target.tagName.toLowerCase() === 'path' && e.target.style.fill !== 'red'){
+             e.target.style.fill = 'red';
+        }else{
+         e.target.style.fill = 'black'
+        }  
+}
+cardContainer.addEventListener('click' , handleCardControllers);
+
+//Handle delete icon click
+const handleProductDelete = (e) => {
+   if(e.target.tagName.toLowerCase() === 'span'){
+    productToDelete = e.target.parentNode.parentNode.parentNode.parentNode;
+    modal.style.display = 'block';
+   }
+}
+cardContainer.addEventListener('click' , handleProductDelete);
+
+//Handle delete and cancel button click
+const handleDeleteBtn = (e) => {
+    if(e.target.tagName.toLowerCase() === 'button' && e.target.className === 'delete-btn'){
+        modal.style.display = 'none';
+    cardContainer.removeChild(productToDelete);
+    }else{
+        modal.style.display = 'none';
+    }
+    
+}
+deleteBtn.addEventListener('click' , handleDeleteBtn);
+cancelBtn.addEventListener('click' , handleDeleteBtn);
